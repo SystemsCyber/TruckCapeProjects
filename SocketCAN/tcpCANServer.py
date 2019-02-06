91fb0c308ac1a4bb769274dbdaac1d403f9d10d5
Fusick <blake-fusick@utulsa.edu>
2019-02-06 00:31:18 -0600

    Create tcpCANServer.py
    Upload of tcpCANServer.

---
+++ SocketCAN/tcpCANServer.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+import time
+import socket
+import struct
+import sys
+
+# Open a socket and bind to it from SocketCAN
+sock = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
+
+# The interface can be seen from the command prompt (ifconfig)
+# The can channel must be configured using the ip link commands
+interface = "can1"
+
+# Bind to the interface
+try:
+    sock.bind((interface,))
+except OSError:
+    print("Could not bind to interface '%s'\n" % interface)
+    sys.exit()
+
+# The basic CAN frame structure and the sockaddr structure are defined
+#   in include/linux/can.h:
+#     struct can_frame {
+#             canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+#             __u8    can_dlc; /* frame payload length in byte (0 .. 8) */
+#             __u8    __pad;   /* padding */
+#             __u8    __res0;  /* reserved / padding */
+#             __u8    __res1;  /* reserved / padding */
+#             __u8    data[8] __attribute__((aligned(8)));
+#     };   
+
+# To match this data structure, the following struct format can be used:
+can_frame_format = "<LL8s"
+# Unsigned Long Integer (little endian), unsigned char (byte), three pad bytes, eight chars (bytes)
+# Note: this is 16 bytes
+
+
+# These are defined in can.h of the Linux kernel sources
+# e.g. https://github.com/torvalds/linux/blob/master/include/uapi/linux/can.h
+"""print("socket.CAN_EFF_MASK = 0x{:08X}".format(socket.CAN_EFF_MASK))
+print("socket.CAN_EFF_FLAG = 0x{:08X}".format(socket.CAN_EFF_FLAG))
+print("socket.CAN_RTR_FLAG = 0x{:08X}".format(socket.CAN_RTR_FLAG))
+print("socket.CAN_ERR_FLAG = 0x{:08X}".format(socket.CAN_ERR_FLAG))"""
+
+#setup tcp server for CAN data reception
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 2319
+BUFFER_SIZE = 1489 #maximum amount of data to receive at once for 93 CAN frames and 1 counter byte 16*93+1 = 1489
+SIZE_OF_CAN_FRAME = 16
+COUNTER_OFFSET = 1 #number of bytes at the start of ethernet packet not part of a CAN frame
+print('---------------------------------------------------------------------')
+print("\nHosting TCP server for CAN data at IP Address {} on Port {}".format(SERVER_IP, SERVER_PORT))
+print('---------------------------------------------------------------------')
+
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	tcpSocket.bind((SERVER_IP, SERVER_PORT))
+except OSError:
+	print("Could not bind TCP Socket.")
+	sys.exit()
+tcpSocket.listen(1)
+conn, addr = tcpSocket.accept()
+print("Connection Address:",addr)
+
+while True:
+	ethData = conn.recv(BUFFER_SIZE)
+	if not ethData: break;
+	print("Received", int(ethData[0]), "CAN frames.")
+	for i in range(int(ethData[0])):
+		#gets individiual CAN packet
+		can_packet = ethData[SIZE_OF_CAN_FRAME*i + COUNTER_OFFSET: SIZE_OF_CAN_FRAME*i + SIZE_OF_CAN_FRAME + COUNTER_OFFSET]
+		can_id, can_dlc_and_microTimer, can_data = struct.unpack(can_frame_format, can_packet)
+
+		can_dlc = (can_dlc_and_microTimer & 0x000000FF)  #first byte is dlc b/c little endian
+		can_microTimer = (can_dlc_and_microTimer & 0xFFFFFF00) >> 8 #first three bytes are microseconds timestamp
+
+		extended_frame = bool(can_id & socket.CAN_EFF_FLAG)
+		error_frame = bool(can_id & socket.CAN_ERR_FLAG)
+		remote_tx_req_frame = bool(can_id & socket.CAN_RTR_FLAG)
+	
+		if error_frame:
+			can_id &= socket.CAN_ERR_MASK
+			can_id_string = "{:08X} (ERROR)".format(can_id)
+		else: #Data Frame
+			if extended_frame:
+				can_id &= socket.CAN_EFF_MASK
+				can_id_string = "{:08X}".format(can_id)
+			else: #Standard Frame
+				can_id &= socket.CAN_SFF_MASK
+				can_id_string = "{:03X}".format(can_id)
+		
+		if remote_tx_req_frame:
+			can_id_string = "{:08X} (RTR)".format(can_id)
+	
+		hex_data_print = ' '.join(["{:02X}".format(can_byte) for can_byte in can_data])
+		print("{:12.2f} {} [{}] {}".format(can_microTimer, can_id_string, can_dlc, hex_data_print))
+conn.close()
