6cdf676bb88310893c7fdbbb7401f9622d20bef0
Fusick <blake-fusick@utulsa.edu>
2019-02-06 23:48:40 -0600

    Update tcpCANServer.py
    Added logging to text file on server side as formated line for each CAN frame.

--- SocketCAN/tcpCANServer.py
+++ SocketCAN/tcpCANServer.py
@@ -46,6 +46,7 @@ SERVER_PORT = 2319
 BUFFER_SIZE = 1425 #maximum amount of data to receive at once for 89 CAN frames and 1 counter byte 16*89+1 = 1425
 SIZE_OF_CAN_FRAME = 16
 COUNTER_OFFSET = 1 #number of bytes at the start of ethernet packet not part of a CAN frame
+LOG_FILE_NAME = 'canlog.txt'
 print('---------------------------------------------------------------------')
 print("\nHosting TCP server for CAN data at IP Address {} on Port {}".format(SERVER_IP, SERVER_PORT))
 print('---------------------------------------------------------------------')
@@ -61,36 +62,38 @@ tcpSocket.listen(1)
 conn, addr = tcpSocket.accept()
 print("Connection Address:",addr)
 
-while True:
-	ethData = conn.recv(BUFFER_SIZE)
-	if not ethData: break;
-	print("Received", int(ethData[0]), "CAN frames.")
-	for i in range(int(ethData[0])):
-		#gets individiual CAN packet
-		can_packet = ethData[SIZE_OF_CAN_FRAME*i + COUNTER_OFFSET: SIZE_OF_CAN_FRAME*i + SIZE_OF_CAN_FRAME + COUNTER_OFFSET]
-		can_id, can_dlc_and_microTimer, can_data = struct.unpack(can_frame_format, can_packet)
+with open (LOG_FILE_NAME, 'w') as file:
+	while True:
+		ethData = conn.recv(BUFFER_SIZE)
+		if not ethData: break;
+		print("Received", int(ethData[0]), "CAN frames.")
+		for i in range(int(ethData[0])):
+			#gets individiual CAN packet
+			can_packet = ethData[SIZE_OF_CAN_FRAME*i + COUNTER_OFFSET: SIZE_OF_CAN_FRAME*i + SIZE_OF_CAN_FRAME + COUNTER_OFFSET]
+			can_id, can_dlc_and_microTimer, can_data = struct.unpack(can_frame_format, can_packet)
 
-		can_dlc = (can_dlc_and_microTimer & 0x000000FF)  #first byte is dlc b/c little endian
-		can_microTimer = (can_dlc_and_microTimer & 0xFFFFFF00) >> 8 #first three bytes are microseconds timestamp
+			can_dlc = (can_dlc_and_microTimer & 0x000000FF)  #first byte is dlc b/c little endian
+			can_microTimer = (can_dlc_and_microTimer & 0xFFFFFF00) >> 8 #first three bytes are microseconds timestamp
 
-		extended_frame = bool(can_id & CAN_EFF_FLAG)
-		error_frame = bool(can_id & CAN_ERR_FLAG)
-		remote_tx_req_frame = bool(can_id & CAN_RTR_FLAG)
+			extended_frame = bool(can_id & CAN_EFF_FLAG)
+			error_frame = bool(can_id & CAN_ERR_FLAG)
+			remote_tx_req_frame = bool(can_id & CAN_RTR_FLAG)
 	
-		if error_frame:
-			can_id &= CAN_ERR_MASK
-			can_id_string = "{:08X} (ERROR)".format(can_id)
-		else: #Data Frame
-			if extended_frame:
-				can_id &= CAN_EFF_MASK
-				can_id_string = "{:08X}".format(can_id)
-			else: #Standard Frame
-				can_id &= CAN_SFF_MASK
-				can_id_string = "{:03X}".format(can_id)
+			if error_frame:
+				can_id &= CAN_ERR_MASK
+				can_id_string = "{:08X} (ERROR)".format(can_id)
+			else: #Data Frame
+				if extended_frame:
+					can_id &= CAN_EFF_MASK
+					can_id_string = "{:08X}".format(can_id)
+				else: #Standard Frame
+					can_id &= CAN_SFF_MASK
+					can_id_string = "{:03X}".format(can_id)
 		
-		if remote_tx_req_frame:
-			can_id_string = "{:08X} (RTR)".format(can_id)
+			if remote_tx_req_frame:
+				can_id_string = "{:08X} (RTR)".format(can_id)
 	
-		hex_data_print = ' '.join(["{:02X}".format(can_byte) for can_byte in can_data])
-		print("{:12.2f} {} [{}] {}".format(can_microTimer, can_id_string, can_dlc, hex_data_print))
-conn.close()
+			hex_data_print = ' '.join(["{:02X}".format(can_byte) for can_byte in can_data])
+			print("{:12.2f} {} [{}] {}".format(can_microTimer, can_id_string, can_dlc, hex_data_print))
+			file.write("{:12.2f} {} [{}] {}\n".format(can_microTimer, can_id_string, can_dlc, hex_data_print))
+	conn.close()
