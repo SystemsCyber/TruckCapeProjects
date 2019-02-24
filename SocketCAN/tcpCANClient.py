901d7f915fe7606893bfffc6d60af012095c837b
Fusick <blake-fusick@utulsa.edu>
2019-02-23 19:59:13 -0600

    Started command server/client basic structure.
    Created initial structure for command line options to send commands with client and accept requests with server. Intermediate step to accepting commands on the server.

--- SocketCAN/tcpCANClient.py
+++ SocketCAN/tcpCANClient.py
@@ -1,159 +1,74 @@
 #!/usr/bin/python3
 
-import time
 import socket
 import sys
-import select
 
-# Open a socket and bind to it from SocketCAN
-canSock = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
-canSock.setsockopt(socket.SOL_CAN_RAW, socket.CAN_RAW_ERR_FILTER, socket.CAN_ERR_MASK) #allows socket to receive error frames
-# The interface can be seen from the command prompt (ifconfig)
-# The can channel must be configured using the ip link commands
-interface = input("Pick an interface! ( can1 | can0 ): ")
-if interface.strip('\n') != 'can0': interface = 'can1' #forces can1 if not can0
+def printHelp():
+    print("\n\nUsage: python3 tcpCANClient.py <CAN interface> <CAN command> [parameters]\n\n")
+    print("NOTE: No parameters must be given for commands with parameter = NONE.\n\n")
+    print("CAN Interfaces: can0")
+    print("                can1")
+    print("                any (all CAN interfaces listed above, limits on commands may apply))\n")
+    print("CAN Commands:   rxon        (turns TCP rx messages on)             [parameter = NONE]")
+    print("                rxoff       (turns TCP rx messages off)            [parameter = NONE]")
+    print("                txon        (turns TCP tx messages on)             [parameter = NONE]")
+    print("                txoff       (turns TCP tx messages off)            [parameter = NONE]")
+    print("                down        (puts CAN interface down)              [parameter = NONE]")
+    print("                up          (puts CAN interface up)                [parameter = NONE]")
+    print("                reset       (down and up command)                  [parameter = NONE]")
+    print("                setbitrate  (sets bitrate of interface)            [parameter = bitrate value]")
+    print("                busload     (estimates busload using can-utils)    [parameter = NONE]")
+    print("                rxmsgs      (shows number of messages received)    [parameter = NONE]")
+    print("                txmsgs      (shows number of messages transmitted) [parameter = NONE]")
+    print("\n")
+    print("rxon/txon/rxoff/txoff tell the server to open/close ports to receive (rx) or transmit (tx) CAN messages\n")
+    print("rxmsgs/txmsgs show number of messages received/transmitted since the TCP connection was started.")
+    print("rxmsgs/txmsgs will return 0 if ports are down or if no messages have been received/transmitted.\n")
 
-print('---------------------------------------------------------------------')
-print("Opening CAN Socket on {}.".format(interface))
-# Bind to the interface
-try:
-    canSock.bind((interface,))
-except OSError:
-    print("Could not bind to interface '%s'\n" % interface)
-    sys.exit()
-
-#setup tcp client for CAN data transfer
-SERVER_IP = "127.0.0.1" #insert IP address of server here
+SERVER_IP = "127.0.0.1"
 SERVER_PORT = 2319
 
-print("Streaming CAN data to IP Address {} on Port {}.", SERVER_IP, SERVER_PORT)
-print('---------------------------------------------------------------------')
+canInterfaces = {'can0': b'\x00', 'can1': b'\x01', 'any': b'\xFF'} #dictionary mapping interface name to byte value for tcp packet
+
+canCommands = {'rxon':   [b'\x00',0],  'rxoff':  [b'\x01',0], 'txon':       [b'\x02',0], 'txoff':   [b'\x03',0], 'down':   [b'\x04',0],\
+               'up' :    [b'\x05',0],   'reset': [b'\x06',0], 'setbitrate': [b'\x07',3], 'busload': [b'\x08',0], 'rxmsgs': [b'\x09',0],
+               'txmsgs': [b'\x10',0]} #dictionary correlating command line abbreviation with list of byte value of tcp packet to send and the number of bytes for parameters
+
+try:
+    if len(sys.argv) > 2 and len(sys.argv) < 5: #if there are two arguments but no more than 4, assuming 1 parameter
+        canIntf = sys.argv[1]
+        canComm = sys.argv[2]
+        if canCommands[canComm][1] > 0:
+            parameter = int(sys.argv[3]).to_bytes(canCommands[canComm][1], byteorder = 'big') #sets parameter bytes of message based off number of bytes of parameters
+        else:
+            if len(sys.argv) > 3: #parameters given when none should be given
+                printHelp()
+                sys.exit()
+            parameter = b'\x00'
+
+        ethData = canInterfaces[canIntf] + canCommands[canComm][0] + parameter #bytes of message to send
+        print("Sending {}".format(ethData))
+    else: #not enough or too many arguments
+        printHelp()
+        sys.exit()
+except KeyError: # no command/interface exists
+    printHelp()
+    sys.exit()
+except IndexError:
+    printHelp()
+    sys.exit()
+
+
+
+print("Sending Control Message to IP Address {} on Port {}.", SERVER_IP, SERVER_PORT)
 
 tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    tcpSocket.connect((SERVER_IP, SERVER_PORT))
+    pass #tcpSocket.connect((SERVER_IP, SERVER_PORT))
 except OSError:
     print("Could not connect TCP Socket. Make sure SERVER_IP is correct.")
     sys.exit()
 
-#initilization of looping parameters TCP packet/Ethernet frame refering to same message
-numCANFrames = 0 #used to store number of CAN frames in tcp packet
-ethData = b'' #used to store payload of ethernet frame
-MIN_ETH_PAYLOAD = 46 #min size of eth packet
-TIMEOUT = 5 #amount of time in seconds allowed to not receive a CAN Frame
-MAX_CAN_PER_TCP = 89 #the maxiumum number of allowed can frames in each tcp packet
-###  NOTE: The client will shutdown if the timeout is reached, otherwise it will keep sending 
-###        only the maximum amount of CAN frames in each tcp packet. MAX_CAN_PER_TCP maxes out at
-###        89 CAN frames since the number of bytes of the payload in the packet is 1424, and the
-###        max size of an ethernet payload is 1500 bytes and each CAN frame takes 16 bytes.
-###        IP header takes 20 bytes and TCP header with options => 40 bytes to be safe. Total bytes = 1500
-startTime = time.time() #start time of data transfer
-currentTime = time.time() #current time in seconds
-lastCANReadTime = time.time() #stores the time of the last read message starting at current time
 
-SEC_TO_MICROS = 1000000 #converts seconds to micros by multiplication
 
-#loop for sending tcp packets
-
-print(currentTime, " ", lastCANReadTime)
-
-while currentTime - lastCANReadTime < TIMEOUT: #loops until the timeout of CAN messages occurs
-    currentTime = time.time()
-    while select.select([canSock],[],[],1)[0]: #while a CAN frame is on the interface
-        canFrame = canSock.recv(16) #just append the raw bytes from the socket, no need to unpack on this end
-        numCANFrames += 1 #increment num of CAN frame counter
-        lastCANReadTime = time.time() #resets timer to indicate message just was read
-        if int((lastCANReadTime - startTime)*SEC_TO_MICROS) >= 0xFFFFFF: startTime = lastCANReadTime #resets start time to rollover timestamp
-        microTimer = (int((lastCANReadTime - startTime)*SEC_TO_MICROS)).to_bytes(3, byteorder='little')
-        ethData += canFrame[:5] + microTimer + canFrame[8:] #replaces padded bytes with microsecond timer
-        if (numCANFrames >= MAX_CAN_PER_TCP):
-            ethData = bytes([numCANFrames]) + ethData #concatenates size as first byte of frame
-            print("Sending full frame")
-            tcpSocket.send(ethData)
-            ethData = b''
-            numCANFrames = 0
-while len(ethData) < MIN_ETH_PAYLOAD: #more than enough padding
-    ethData += b'\x00' #pads frame to min size of ethernet frame
-print("Sending padded frame [FINAL]")
-tcpSocket.send(ethData)
-tcpSocket.close()#!/usr/bin/python3
-
-import time
-import socket
-import sys
-import select
-
-# Open a socket and bind to it from SocketCAN
-canSock = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
-sock.setsockopt(socket.SOL_CAN_RAW, socket.CAN_RAW_ERR_FILTER, socket.CAN_ERR_MASK) #allows socket to receive error frames
-# The interface can be seen from the command prompt (ifconfig)
-# The can channel must be configured using the ip link commands
-interface = input("Pick an interface! ( can1 | can0 ): ")
-if interface.strip('\n') != 'can0': interface = 'can1' #forces can1 if not can0
-
-print('---------------------------------------------------------------------')
-print("Opening CAN Socket on {}.".format(interface))
-# Bind to the interface
-try:
-    canSock.bind((interface,))
-except OSError:
-    print("Could not bind to interface '%s'\n" % interface)
-    sys.exit()
-
-#setup tcp client for CAN data transfer
-SERVER_IP = "127.0.0.1" #insert IP address of server here
-SERVER_PORT = 2319
-
-print("Streaming CAN data to IP Address {} on Port {}.", SERVER_IP, SERVER_PORT)
-print('---------------------------------------------------------------------')
-
-tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    tcpSocket.connect((SERVER_IP, SERVER_PORT))
-except OSError:
-    print("Could not connect TCP Socket. Make sure SERVER_IP is correct.")
-    sys.exit()
-
-#initilization of looping parameters TCP packet/Ethernet frame refering to same message
-numCANFrames = 0 #used to store number of CAN frames in tcp packet
-ethData = b'' #used to store payload of ethernet frame
-MIN_ETH_PAYLOAD = 46 #min size of eth packet
-TIMEOUT = 5 #amount of time in seconds allowed to not receive a CAN Frame
-MAX_CAN_PER_TCP = 89 #the maxiumum number of allowed can frames in each tcp packet
-###  NOTE: The client will shutdown if the timeout is reached, otherwise it will keep sending 
-###        only the maximum amount of CAN frames in each tcp packet. MAX_CAN_PER_TCP maxes out at
-###        89 CAN frames since the number of bytes of the payload in the packet is 1424, and the
-###        max size of an ethernet payload is 1500 bytes and each CAN frame takes 16 bytes.
-###        IP header takes 20 bytes and TCP header with options => 40 bytes to be safe. Total bytes = 1500
-startTime = time.time() #start time of data transfer
-currentTime = time.time() #current time in seconds
-lastCANReadTime = time.time() #stores the time of the last read message starting at current time
-
-SEC_TO_MICROS = 1000000 #converts seconds to micros by multiplication
-
-#loop for sending tcp packets
-
-print(currentTime, " ", lastCANReadTime)
-
-while currentTime - lastCANReadTime < TIMEOUT: #loops until the timeout of CAN messages occurs
-    currentTime = time.time()
-    while select.select([canSock],[],[],1)[0]: #while a CAN frame is on the interface
-        canFrame = canSock.recv(16) #just append the raw bytes from the socket, no need to unpack on this end
-        numCANFrames += 1 #increment num of CAN frame counter
-        lastCANReadTime = time.time() #resets timer to indicate message just was read
-        if int((lastCANReadTime - startTime)*SEC_TO_MICROS) >= 0xFFFFFF: startTime = lastCANReadTime #resets start time to rollover timestamp
-        microTimer = (int((lastCANReadTime - startTime)*SEC_TO_MICROS)).to_bytes(3, byteorder='little')
-        ethData += canFrame[:5] + microTimer + canFrame[8:] #replaces padded bytes with microsecond timer
-        if (numCANFrames >= MAX_CAN_PER_TCP):
-            ethData = bytes([numCANFrames]) + ethData #concatenates size as first byte of frame
-            print("Sending full frame")
-            tcpSocket.send(ethData)
-            ethData = b''
-            numCANFrames = 0
-while len(ethData) < MIN_ETH_PAYLOAD: #more than enough padding
-    ethData += b'\x00' #pads frame to min size of ethernet frame
-print("Sending padded frame [FINAL]")
-tcpSocket.send(ethData)
-tcpSocket.close()
